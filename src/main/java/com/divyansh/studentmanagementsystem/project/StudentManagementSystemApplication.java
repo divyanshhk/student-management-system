abb5db75d62fd53867e9db4e11a661ef5e42c4ff
Divyansh <kushwahadivyansh08@gmail.com>
2026-04-11 10:24:23 +0530

    final push for backend

--- src/main/java/com/divyansh/studentmanagementsystem/project/StudentManagementSystemApplication.java
+++ src/main/java/com/divyansh/studentmanagementsystem/project/StudentManagementSystemApplication.java
@@ -8,8 +8,6 @@ public class StudentManagementSystemApplication {
 
 
 	public static void main(String[] args) {
-		System.out.println("DB_URL = " + System.getenv("DB_URL"));
-
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
 	}
 
