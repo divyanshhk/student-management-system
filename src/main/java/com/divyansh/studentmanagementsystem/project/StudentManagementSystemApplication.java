ed32c0b58bdfd0e49b070a3a81bbe4ae2e81ed07
Divyansh <kushwahadivyansh08@gmail.com>
2026-04-11 08:47:46 +0530

    application.properties updated due to error log check

--- src/main/java/com/divyansh/studentmanagementsystem/project/StudentManagementSystemApplication.java
+++ src/main/java/com/divyansh/studentmanagementsystem/project/StudentManagementSystemApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudentManagementSystemApplication {
 
+
 	public static void main(String[] args) {
+		System.out.println("DB_URL = " + System.getenv("DB_URL"));
+
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
 	}
 
